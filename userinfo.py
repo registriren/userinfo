3c241151f810f162ef89477083c1cf7c42b192f0
registriren <50903184+registriren@users.noreply.github.com>
2019-08-15 16:57:29 +1000

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -23,11 +23,12 @@ def main():
         user_id = bot.get_user_id(last_update)
         name = bot.get_name(last_update)
         link_userid = bot.get_link_user_id(last_update)
+        link_name = bot.get_link_name(last_update)
 
         if link_userid == None:
             bot.send_message('name: {}\nuser_id: {}\nchat_id: {}\ntype: {}\n'.format(name, user_id, chat_id, type_upd, link_userid), chat_id)
         else:
-            bot.send_message('forward name: {}\nforward user_id: {}\nchat_id: {}\ntype: {}\n'.format(name, link_userid, chat_id, type_upd), chat_id)
+            bot.send_message('forward name: {}\nforward user_id: {}\nchat_id: {}\ntype: {}\n'.format(link_name, link_userid, chat_id, type_upd), chat_id)
         bot.send_message('полная структура ответа GetUpdates:\n{}'.format(str(last_update)), chat_id)
         
 if __name__ == '__main__':
